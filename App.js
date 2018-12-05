7d1b8d83094642a8c31b83286d2fcf2770fa2b1f
Baskara Reddy <baskara.ar@gmail.com>
2018-12-05 11:06:41 -0800

    added touchable tiles

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+//import Image from 'react-native-scalable-image';
 
 import {
   View,
@@ -27,14 +28,17 @@ class App extends React.Component {
   state = {
     modalVisible: false,
     photos: [],
-    index: null
+    index: null,
+    fullscreen: false
   }
 
   setIndex = (index) => {
     if (index === this.state.index) {
       index = null
     }
-    this.setState({ index })
+    //debugger;
+    fullscreen = true
+    this.setState({ index, fullscreen })
   }
 
   getPhotos = () => {
@@ -49,30 +53,18 @@ class App extends React.Component {
     this.setState({ modalVisible: !this.state.modalVisible });
   }
 
-  navigate = () => {
+  toggleFullScreen = () => {
+    this.setState({ fullscreen: !this.state.fullscreen });
+  }
+
+  navigater = () => {
+    //debugger;
     const { navigate } = this.props.navigation
     navigate('ImageBrowser')
   }
 
-  share = () => {
-    const image = this.state.photos[this.state.index].node.image.uri
-    RNFetchBlob.fs.readFile(image, 'base64')
-    .then((data) => {
-      let shareOptions = {
-        title: "React Native Share Example",
-        message: "Check out this photo!",
-        url: `data:image/jpg;base64,${data}`,
-        subject: "Check out this photo!"
-      }
-
-      Share.open(shareOptions)
-        .then((res) => console.log('res:', res))
-        .catch(err => console.log('err', err))
-    })
-  }
-
   render() {
-    console.log('state :', this.state)
+    console.log('state in render :', this.state)
     return (
       <View style={styles.container}>
         <Button
@@ -81,35 +73,37 @@ class App extends React.Component {
         />
         <Button
           title='Browse Images'
-          onPress={this.navigate}
+          onPress={this.navigater}
         />
-        <Modal
-          animationType={"slide"}
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => console.log('closed')}
-        >
+        <Modal 
+           animationType={"slide"} 
+           transparent={false} 
+           visible={this.state.modalVisible} 
+           onRequestClose={() => console.log('closed')}>
           <View style={styles.modalContainer}>
             <Button
               title='Close'
-              onPress={this.toggleModal}
+              onPress={() => {this.toggleModal(); this.toggleFullScreen()}}
             />
             <ScrollView
-              contentContainerStyle={styles.scrollView}>
+             contentContainerStyle={styles.scrollView}>
               {
-                this.state.photos.map((p, i) => {
+                
+                this.state.photos.map((p, i) => 
+                {
                   return (
-                    <TouchableHighlight
-                      style={{opacity: i === this.state.index ? 0.5 : 1}}
+                   <TouchableHighlight
+                      style={{opacity: 1}}
                       key={i}
                       underlayColor='transparent'
-                      onPress={() => this.setIndex(i)}
+                      onPress={() => {this.setIndex(i); this.toggleFullScreen()}}
                     >
                       <Image
-                        style={{
-                          width: width/3,
-                          height: width/3
+                         style={{
+                          width: this.state.fullscreen === true ? width : width/3,
+                          height: this.state.fullscreen === true ? width : width/3
                         }}
+                        //resizeMode={'contain'}
                         source={{uri: p.node.image.uri}}
                       />
                     </TouchableHighlight>
@@ -117,16 +111,6 @@ class App extends React.Component {
                 })
               }
             </ScrollView>
-            {
-              this.state.index !== null  && (
-                <View style={styles.shareButton}>
-                  <Button
-                      title='Share'
-                      onPress={this.share}
-                    />
-                </View>
-              )
-            }
           </View>
         </Modal>
       </View>
@@ -138,22 +122,17 @@ styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#999'
   },
   modalContainer: {
     paddingTop: 20,
+    //backgroundColor: '#fc0',
     flex: 1
   },
   scrollView: {
     flexWrap: 'wrap',
     flexDirection: 'row'
-  },
-  shareButton: {
-    position: 'absolute',
-    width,
-    padding: 10,
-    bottom: 0,
-    left: 0
   }
 })
 
